4e5e6d846d68a1301d4a2b332f0f3530a211ab5a
and47 <and47@users.noreply.github.com>
2021-06-29 07:34:59 +0200

    Generated HTML (takes about 15 mins) following the URL fix.

--- R/eda_funs.r
+++ R/eda_funs.r
@@ -147,28 +147,3 @@ rateResponses <- function(sveyRes, idxset, weights = 1L, idxcol = 'idx', onlyInt
   sveyRes %>% mutate(across(everything(), as.integer)) %>%
    {bind_cols(respondents, . * weights * sveyRespIdxs, rtng = rowMeans(sveyRespIdxs, na.rm = TRUE))}
 }
-
-#'
-#' @export
-runConjoint <- function(dataset, fctCols, yCol) {
- prfls <- unique(dataset[,fctCols])
- prfls_int <- mutate(prfls, across(everything(), ~as.integer(as.factor(.))))
- dataset_chr <- dataset[,fctCols] %>% transmute(across(everything(), as.character))
- allLvls <- dataset_chr %>% as.matrix() %>% as.character() %>% unique()
-
- dataset$fctCombo <- match(x = as.character(tidyr::unite(dataset_chr,'')[[1]]),
-                           table = as.character(tidyr::unite(prfls,'')[[1]]))
- col1nm <- names(dataset)[1]  # first col is assumed to be respondent's id
- datasetY <- dataset[, c(col1nm, 'fctCombo', yCol)]  # dplyr::pivot_wide requires unnese/unchop %>%
- #  group_by(!!as.name(col1nm)) %>% mutate(grouped_id = row_number()) %>%
- #  tidyr::spread(key = 'fctCombo', value = yCol, convert = TRUE) %>% dplyr::select(-grouped_id) %>%
- #  group_by(!!as.name(col1nm)) %>%
- #  summarise(across(everything(), ~ first(na.omit(.))))
- Y.wd <- matrix(nrow = length(unique(dataset[[col1nm]])), ncol = nrow(prfls),
-                dimnames = list(unique(dataset[[col1nm]]), NULL))
- for (i in 1:nrow(datasetY)) Y.wd[datasetY[[col1nm]][i], datasetY$fctCombo[i]] <- datasetY[[yCol]][i]
-
- Conjoint(y = as.data.frame(Y.wd),
-          x = as.data.frame(prfls_int),
-          z = as.data.frame(allLvls, stringsAsFactors = TRUE))
-}
